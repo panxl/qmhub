7c68c31657742a6e7b7734a82fa8e71af53b7904
panxl <panxl@users.noreply.github.com>
2019-09-27 11:40:32 -0500

    Small update

--- qmhub/utils.py
+++ qmhub/utils.py
@@ -129,45 +129,77 @@ class DependArray(container):
         return self
 
 
-def get_numerical_gradient(system, property, gradient, over='i', i=None, j=None, k=None):
+def get_numerical_gradient(system, property, gradient, mask=None, over='i', i=None, j=None, k=None):
     ndim = property.ndim
-
-    if over == 'i':
-        if ndim == 2:
-            analytical_gradient = -np.asscalar(gradient[k, i, j])
-        elif ndim == 1:
-            if gradient.ndim == 3:
-                analytical_gradient = -np.asscalar(gradient[k, i, :].sum())
-            if gradient.ndim == 2:
-                analytical_gradient = -np.asscalar(gradient[k, i].sum())
-
-        system.atoms.positions[k, i:i+1] += 0.001
-        property_pos = np.copy(property)
-
-        system.atoms.positions[k, i:i+1] -= 0.002
-        property_neg = np.copy(property)
-
-        system.atoms.positions[k, i:i+1] += 0.001
-    elif over == 'j':
-        if ndim == 2:
-            analytical_gradient = np.asscalar(gradient[k, i, j])
-        elif ndim == 1:
-            analytical_gradient = np.asscalar(gradient[k, :, j].sum())
-
-        system.atoms.positions[k, j:j+1] += 0.001
-        property_pos = np.copy(property)
-
-        system.atoms.positions[k, j:j+1] -= 0.002
-        property_neg = np.copy(property)
-
-        system.atoms.positions[k, j:j+1] += 0.001
+    if mask is not None:
+        j2 = np.where(mask)[0][j]
+    else:
+        j2 = j
 
     if ndim == 2:
-        numerical_gradient = (property_pos[i, j] - property_neg[i, j]) / 0.002
-    elif ndim == 1:
         if over == 'i':
-            numerical_gradient = (property_pos[i] - property_neg[i]) / 0.002
-        if over == 'j':
-            numerical_gradient = (property_pos[j] - property_neg[j]) / 0.002
+            analytical_gradient = -np.asscalar(gradient[k, i, j])
+
+            system.atoms.positions[k, i:i+1] += 0.001
+            property_pos = np.copy(property)
+
+            system.atoms.positions[k, i:i+1] -= 0.002
+            property_neg = np.copy(property)
+
+            system.atoms.positions[k, i:i+1] += 0.001
+        elif over == 'j':
+            analytical_gradient = np.asscalar(gradient[k, i, j])
+
+            system.atoms.positions[k, j2:j2+1] += 0.001
+            property_pos = np.copy(property)
+
+            system.atoms.positions[k, j2:j2+1] -= 0.002
+            property_neg = np.copy(property)
+
+            system.atoms.positions[k, j2:j2+1] += 0.001
+
+        numerical_gradient = (property_pos[i, j] - property_neg[i, j]) / 0.002
+
+    if ndim == 1:
+        if len(property) == len(system.qm.atoms):
+            if over == 'i':
+                analytical_gradient = -np.asscalar(gradient[k, i])
+
+                system.atoms.positions[k, i:i+1] += 0.001
+                property_pos = np.copy(property)
+
+                system.atoms.positions[k, i:i+1] -= 0.002
+                property_neg = np.copy(property)
+
+                system.atoms.positions[k, i:i+1] += 0.001
+
+                numerical_gradient = (property_pos[i] - property_neg[i]) / 0.002
+            elif over == 'j':
+                return
+        else:
+            if over == 'i':
+                analytical_gradient = -np.asscalar(gradient[k, i, j])
+
+                system.atoms.positions[k, i:i+1] += 0.001
+                property_pos = np.copy(property)
+
+                system.atoms.positions[k, i:i+1] -= 0.002
+                property_neg = np.copy(property)
+
+                system.atoms.positions[k, i:i+1] += 0.001
+
+                numerical_gradient = (property_pos[j] - property_neg[j]) / 0.002
+            elif over == 'j':
+                analytical_gradient = np.asscalar(gradient[k, :, j].sum())
+
+                system.atoms.positions[k, j2:j2+1] += 0.001
+                property_pos = np.copy(property)
+
+                system.atoms.positions[k, j2:j2+1] -= 0.002
+                property_neg = np.copy(property)
+
+                system.atoms.positions[k, j2:j2+1] += 0.001
+
+                numerical_gradient = (property_pos[j] - property_neg[j]) / 0.002
 
     return analytical_gradient, numerical_gradient, analytical_gradient - numerical_gradient
