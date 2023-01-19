e5695f986de7af03ff5e9135e087bc8a2f7d144f
DanielSchnabel <danielschnabel2@gmail.com>
2023-01-19 14:44:53 -0600

    Update system.py

--- qmhub/system.py
+++ qmhub/system.py
@@ -5,7 +5,20 @@ from .atoms import Atoms
 
 
 class System(object):
+    '''
+    QMHub System object
+    '''
     def __init__(self, n_atoms, n_qm_atoms, qm_charge, qm_mult):
+        '''
+        Creates system in which the simulation will run. Contains the total number of atoms to run the Molecular Mechanical simulation on, 
+        and the number of atoms to run the Quantum Mechanical simulation on. N.B. The numer of QM atoms cannot be greater than the number of total atoms. 
+        
+        Args:
+            n_atoms (int):
+            n_qm_atoms (int):
+            qm_charge ():
+            qm_mult ():
+        '''
         self.qm_index = np.s_[:n_qm_atoms]
         self.mm_index = np.s_[n_qm_atoms:]
 
@@ -28,6 +41,11 @@ class System(object):
         self.qm_mult = qm_mult
 
     def wrap_positions(self):
+        '''what
+        
+        Args:
+            
+        '''
         self.atoms.positions -= self.qm.atoms.positions.mean(axis=1, keepdims=True)
         if not np.all(self.cell_basis == 0):
             self.atoms.positions -= np.around(self.atoms.positions / np.diagonal(self.cell_basis)[:, np.newaxis]) * np.diagonal(self.cell_basis)[:, np.newaxis]
