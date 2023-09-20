51342ef920f48f5418dcfa14e8887d38a4a12d82
panxl <panxl@users.noreply.github.com>
2023-09-20 16:41:22 -0500

    Update Torch

--- qmhub/qmtools/torch.py
+++ qmhub/qmtools/torch.py
@@ -1,5 +1,9 @@
 import numpy as np
 import torch
+import torch_geometric
+import torch_cluster
+import torch_scatter
+import torch_sparse
 
 from .qmbase import QMBase
 from ..units import CODATA08_BOHR_TO_A, CODATA08_HARTREE_TO_EV
@@ -11,6 +15,7 @@ class Torch(QMBase):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        torch.set_num_threads(self.nproc)
         self._model = torch.jit.load(self.options['model']).double()
 
     def _get_qm_cache(self, *args, output=None):
@@ -21,31 +26,31 @@ class Torch(QMBase):
 
         if self.mm_charges is not None:
             args = (
-                torch.from_numpy(np.ascontiguousarray(self.qm_positions.T)[None]),
                 torch.from_numpy(self.qm_element_ids[:]),
-                torch.from_numpy(self.mm_positions.T[None]),
-                torch.from_numpy(self.mm_charges[None]),
+                torch.from_numpy(np.ascontiguousarray(self.qm_positions.T)),
+                torch.from_numpy(self.mm_positions.T),
+                torch.from_numpy(self.mm_charges[:]),
                 )
         else:
             args = (
-                torch.from_numpy(np.ascontiguousarray(self.qm_positions.T)[None]),
                 torch.from_numpy(self.qm_element_ids[:]),
+                torch.from_numpy(np.ascontiguousarray(self.qm_positions.T)),
                 )
 
         return self._model(*args)
 
     def _get_qm_energy(self, qm_cache):
         """Get QM energy from output of QM calculation."""
-        return qm_cache[0][0].detach().numpy() / CODATA08_HARTREE_TO_EV
+        return qm_cache[0][0][0].detach().numpy() / CODATA08_HARTREE_TO_EV
 
     def _get_qm_energy_gradient(self, qm_cache):
         """Get QM energy gradient from output of QM calculation."""
-        return qm_cache[1][0].detach().numpy().T / (CODATA08_HARTREE_TO_EV / CODATA08_BOHR_TO_A)
+        return -qm_cache[1].detach().numpy().T / (CODATA08_HARTREE_TO_EV / CODATA08_BOHR_TO_A)
 
     def _get_mm_esp(self, qm_cache):
         """Get electrostatic potential at MM atoms in the near field from QM density."""
 
         mm_esp = np.zeros((4, len(self.mm_charges)))
-        mm_esp[0] = qm_cache[2][0].detach().numpy() / CODATA08_HARTREE_TO_EV
-        mm_esp[1:] = qm_cache[3][0].detach().numpy().T / (CODATA08_HARTREE_TO_EV / CODATA08_BOHR_TO_A)
+        mm_esp[0] = qm_cache[2].detach().numpy() / CODATA08_HARTREE_TO_EV
+        mm_esp[1:] = qm_cache[3].detach().numpy().T / (CODATA08_HARTREE_TO_EV / CODATA08_BOHR_TO_A)
         return mm_esp
